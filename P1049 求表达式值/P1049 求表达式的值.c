b2c6e42c5ea55641327506848da9e4800487b76b
cuo-ren <159247363+cuo-ren@users.noreply.github.com>
2024-10-18 18:24:09 +0800

    Update and rename 表达式的值.c to P1049 求表达式值/P1049 求表达式的值.c

--- P1049 求表达式值/P1049 求表达式的值.c
+++ P1049 求表达式值/P1049 求表达式的值.c
@@ -5,7 +5,7 @@ int main()
 	int a;
 	double x, y;
 
-	while (scanf_s("%d%lf%lf", &a, &x, &y) != EOF) {
+	while (scanf("%d%lf%lf", &a, &x, &y) != EOF) {
 		printf("%.6lf\n", x + a % 3 * (int)(x + y) % 2 / 4);
 	}
 }
