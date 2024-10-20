bbd6b9dcb2d17b5cf69c0c5dd425dbe09ce770fd
cuo-ren <159247363+cuo-ren@users.noreply.github.com>
2024-10-20 23:44:53 +0800

    Add files via upload

---
+++ 最大最大公约数和最小公倍数.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c, x, y;
+	
+	while (scanf_s("%d%d", &a, &b) != EOF) {
+		getchar();
+		x = a;
+		y = b;
+		if (a < b) {
+			c = a;
+			a = b;
+			b = c;
+		}
+		while (b != 0) {
+			c = a % b;
+			a = b;
+			b = c;
+		}
+		y = x * y / a;
+		x = a;
+		printf("%d\n%d\n", x, y);
+	}
+}
