09c3668b9a6a99ae7fc8de58df3cec3dd4a803d3
cuo-ren <159247363+cuo-ren@users.noreply.github.com>
2024-10-26 13:55:11 +0800

    Update P1035 鸡兔同笼问题.c

--- P1035 鸡兔同笼问题/P1035 鸡兔同笼问题.c
+++ P1035 鸡兔同笼问题/P1035 鸡兔同笼问题.c
@@ -5,7 +5,6 @@ int main()
 	int m, n, a, b;
 
 	while (scanf("%d%d", &m, &n) != EOF) {
-		getchar();
 		b = (n - 2 * m) / 2;
 		a = (4 * m - n) / 2;
 		if (a >= 0 && b >= 0 && ((n - 2 * m) % 2 == 0) && ((4 * m - n) % 2 == 0)) {
